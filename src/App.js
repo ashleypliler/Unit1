f40824b688bb125c47f97a25a2297ad5a634508d
Ashley Pliler <ashleypliler1@gmail.com>
2023-01-03 21:42:12 -0700

    project part 5

--- src/App.js
+++ src/App.js
@@ -2,36 +2,58 @@ import { useState } from "react";
 import "./App.css";
 import Square from "./Square";
 
+function calculateWinner(arr) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i = 0; i < lines.length; i++){
+    const {a,b,c} = lines[i];
+    if(arr[a] && arr[a] === arr[b] && arr[a] === arr[c]){
+      return `${arr[a]} won!!`
+    }else{
+      return 'Who will win?!'
+    }
+  }
+}
+
 function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
   // const prop = "hi there!";
 
   const handleClick = () => {
-  setSquares(["", "", "", "", "", "", "", "", ""])
-  setPlayer(true)
-}
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
 
   return (
     <div className="App">
-        <div className="container">
-            {squares.map((val, index) => {
-                return (
-                    <Square
-                        setSquares={setSquares}
-                        index={index}
-                        squareValue={val}
-                        squares={squares}
-                        player={player}
-                        setPlayer={setPlayer}
-                    />
-                );
-            })}
-        </div>
-        <button onClick={handleClick}>Reset</button>
+    <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={val}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
-);
+  );
 }
 
 export default App;
-
