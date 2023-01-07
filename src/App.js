6038141d61e09774764469700d5ce13cb3d48057
Ashley Pliler <ashleypliler1@gmail.com>
2023-01-06 18:52:15 -0700

    finished with unit 1

--- src/App.js
+++ src/App.js
@@ -2,41 +2,39 @@ import { useState } from "react";
 import "./App.css";
 import Square from "./Square";
 
-function calculateWinner(arr) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for(let i = 0; i < lines.length; i++){
-    const {a,b,c} = lines[i];
-    if(arr[a] && arr[a] === arr[b] && arr[a] === arr[c]){
-      return `${arr[a]} won!!`
-    }else{
-      return 'Who will win?!'
-    }
-  }
-}
-
 function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
-  // const prop = "hi there!";
 
   const handleClick = () => {
     setSquares(["", "", "", "", "", "", "", "", ""]);
     setPlayer(true);
   };
 
+  function calculateWinner(arr) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c ] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return `${squares[a]} won!!`;
+      }
+    }
+    return "Who will win?!";
+  }
+
   return (
     <div className="App">
-    <span>{calculateWinner(squares)}</span>
+      <span>{calculateWinner(squares)}</span>
       <div className="container">
         {squares.map((val, index) => {
           return (
